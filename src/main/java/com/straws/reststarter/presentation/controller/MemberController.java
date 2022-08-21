7d3002fc15b34074b3f3d9bb6bdd658b05c27630
FooQoo <horseshoes.buzz.17@gmail.com>
2022-08-21 11:43:55 +0900

    add prefix path

--- src/main/java/com/straws/reststarter/presentation/controller/MemberController.java
+++ src/main/java/com/straws/reststarter/presentation/controller/MemberController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/v1")
 @RequiredArgsConstructor
 public class MemberController {
 
